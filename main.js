d12c07e5375e7e1618d57f772001b85a284faaaf
ishiarya <96100229+ishiarya@users.noreply.github.com>
2022-01-15 20:03:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var first_name = "Ishita";
+var last_name = "Kharya";
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = 67
+var b = 46
+var add = a+b
+console.log("67 + 46 =" + add);
